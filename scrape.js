45c2c5c3f59671498fc9e0fd0566e8ece8c3854f
PoohJr <Williamdomenech06@gmail.com>
2025-02-20 10:56:41 -0500

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -3,17 +3,21 @@ import puppeteer from "puppeteer";
 const scrape = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  const url = "https://books.toscrape.com/";
+  await page.goto(url);
 
-  await page.goto("https://books.toscrape.com/");
-
-  await page.locator(".thumbnail")
   const vid = await page.evaluate(() => {
-    const vidtits = document.querySelectorAll("#video-title")
-    return vidtits
-  })
+    const vidtits = document.querySelectorAll(".product_pod");
+    return Array.from(vidtits).map((vid) => {
+      //GET TITLE BY GETTING THE ELEMENTS AND ATTRIUBUTE SINCE NO CLASS
+      const title = vid.querySelector("h3 a").getAttribute("title");
+      //TEXT CONTENT TO GET WHATS BEING SHOWN
+      const price = vid.querySelector(".price_color").textContent;
+      return price;
+    });
+  });
   console.log(vid);
-  
-  
+
   await browser.close();
 };
 
