7b15a9e6b3c9f889577692548f3b142e51adfce1
PoohJr <Williamdomenech06@gmail.com>
2025-02-20 13:57:20 -0500

    nm

--- scrape.js
+++ scrape.js
@@ -1,22 +1,45 @@
 import puppeteer from "puppeteer";
-
+import fs from "fs";
 const scrape = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const url = "https://books.toscrape.com/";
+  const allBooks = [];
+  let currentpage = 1;
+  const maxpg = 3;
+  const url = `https://books.toscrape.com/catalogue/page-${currentpage}.html`;
   await page.goto(url);
 
-  const vid = await page.evaluate(() => {
-    const vidtits = document.querySelectorAll(".product_pod");
-    return Array.from(vidtits).map((vid) => {
-      //GET TITLE BY GETTING THE ELEMENTS AND ATTRIUBUTE SINCE NO CLASS
-      const title = vid.querySelector("h3 a").getAttribute("title");
-      //TEXT CONTENT TO GET WHATS BEING SHOWN
-      const price = vid.querySelector(".price_color").textContent;
-      return price;
+  while (currentpage <= maxpg) {
+    const vid = await page.evaluate(() => {
+      const vidtits = document.querySelectorAll(".product_pod");
+      return Array.from(vidtits).map((vid) => {
+        //GET TITLE BY GETTING THE ELEMENTS AND ATTRIUBUTE SINCE NO CLASS
+        const title = vid.querySelector("h3 a").getAttribute("title");
+        //TEXT CONTENT TO GET WHATS BEING SHOWN
+        const price = vid.querySelector(".price_color").textContent;
+        const stock = vid.querySelector(".instock.availability")
+          ? "In Stock"
+          : "Out of Stock";
+        const rating = vid
+          .querySelector(".star-rating")
+          .className.split(" ")[1];
+        const link = vid.querySelector("h3 a").getAttribute("href");
+        return {
+          title,
+          price,
+          stock,
+          rating,
+          link,
+        };
+      });
     });
-  });
-  console.log(vid);
+
+    allBooks.push(...vid);
+    console.log(`Books on current page ${currentpage}: `, vid);
+    currentpage++;
+  }
+
+  fs.writeFileSync("books.json", JSON.stringify(allBooks, null, 2));
 
   await browser.close();
 };
